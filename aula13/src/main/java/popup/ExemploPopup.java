c195c1368c96fd390c46e8fa79dcff552e7a0e24
Diego Keller <keller.diego@gmail.com>
2016-05-24 19:00:38 -0300

    Componentes Java Swing

---
+++ aula13/src/main/java/popup/ExemploPopup.java
@@ -0,0 +1,52 @@
+package popup;
+
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
+
+import comum.JanelaModelo;
+
+public class ExemploPopup extends JanelaModelo {
+
+	private JPopupMenu pop;
+
+	public ExemploPopup(String titulo, int altura, int largura) {
+		super(titulo, altura, largura);
+
+		JPanel conteudo = new JPanel();
+		conteudo.setLayout(new GridLayout(7, 1));
+		this.add(conteudo);
+		
+		pop = new JPopupMenu();
+		pop.add(new JMenuItem("Salvar"));
+		pop.add(new JMenuItem("Sair"));
+		
+		this.addMouseListener(new PopupListener());
+		
+	}
+
+	public static void main(String[] args) {
+		new ExemploPopup("Popup", 300, 500).setVisible(true);
+	}
+
+	class PopupListener extends MouseAdapter {
+	    public void mousePressed(MouseEvent e) {
+	        deveMostrar(e);
+	    }
+
+	    public void mouseReleased(MouseEvent e) {
+	        deveMostrar(e);
+	    }
+
+	    private void deveMostrar(MouseEvent e) {
+	        if (e.isPopupTrigger()) {
+	            pop.show(e.getComponent(),
+	                       e.getX(), e.getY());
+	        }
+	    }
+	}
+}
